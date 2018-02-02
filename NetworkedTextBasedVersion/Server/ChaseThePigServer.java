437c224cebf931bfeb46baf435e46bc8e8ad6fa4
breanainnc <breanainnc@live.ie>
2018-02-02 19:22:59 +0000

    Create ChaseThePigServer.java

---
+++ NetworkedTextBasedVersion/Server/ChaseThePigServer.java
@@ -0,0 +1,11 @@
+package server;
+/*
+author Breanainn
+*/
+public class ChaseThePigServer{
+    
+    public static void main(String[] args) {
+        SocketServer s = new SocketServer();
+        s.runServer();
+    }
+}
