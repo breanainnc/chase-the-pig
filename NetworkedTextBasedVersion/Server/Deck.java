6382206e4057b9f212d1e7170672eacdaf9edb70
breanainnc <breanainnc@live.ie>
2018-02-02 19:31:27 +0000

    Create Deck.java

---
+++ NetworkedTextBasedVersion/Server/Deck.java
@@ -0,0 +1,24 @@
+package server;
+
+class Deck{
+    int[] cards;
+    
+    public Deck(){
+    this.cards = new int[52];
+    for(int i = 0; i < 52; i++){
+        cards[i] = i;
+    }
+}
+    public void shuffle(){
+        for(int i = 0; i < 52; i++){
+            int random = (int)(Math.random()*52);
+            int temp = cards[i];
+            cards[i] = cards[random];
+            cards[random] = temp;
+        }
+    }
+    public int[] getDeck(){
+        return cards;
+    }
+    
+}
