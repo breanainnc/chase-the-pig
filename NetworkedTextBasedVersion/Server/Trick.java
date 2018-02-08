e69a9e60921e21c71a933fb65f0755b3b6b5a9fb
breanainnc <breanainnc@live.ie>
2018-02-08 20:12:53 +0000

    Update Trick.java

--- NetworkedTextBasedVersion/Server/Trick.java
+++ NetworkedTextBasedVersion/Server/Trick.java
@@ -1,55 +1,107 @@
 package server;
-import java.util.*;
-import java.io.*;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author brean
  */
-public class Trick {
-    ArrayList<Integer> playedCards;
-    int currentWinner;
-    int bestCard;
+class Score {
+    ArrayList<Integer> exposedCards;
+    int[] playerScores;
+    ArrayList<Integer> player1Cards;
+    ArrayList<Integer> player2Cards;
+    ArrayList<Integer> player3Cards;
+    ArrayList<Integer> player4Cards;
     
-    public Trick(){
-        this.playedCards = new ArrayList<>();
+    public Score(){
+        this.exposedCards = new ArrayList<>();
+        this.playerScores = new int[4];
+        for(int i = 0; i < 4; i++){
+            playerScores[i] = 0;
+        }
+        this.player1Cards = new ArrayList<>();
+        this.player2Cards = new ArrayList<>();
+        this.player3Cards = new ArrayList<>();
+        this.player4Cards = new ArrayList<>();
     }
-    
-    public void addCard(int card, int pid){
-        playedCards.add(card);
-        if(playedCards == null){
-            currentWinner = pid;
-            bestCard = card;
+    public void addExCard(int card){
+        exposedCards.add(card);
+    }
+    public void addWonCards(int pid, ArrayList<Integer> cards){
+        if(pid == 0){
+            for(int i = 0; i < 4; i++){
+                player1Cards.add(cards.get(i));
+            }
+        }
+        else if(pid == 1){
+            for(int i = 0; i < 4; i++){
+                player2Cards.add(cards.get(i));
+            }
+        }
+        else if(pid == 2){
+            for(int i = 0; i < 4; i++){
+                player3Cards.add(cards.get(i));
+            }
         }
         else{
-            compareCards(card,pid);
-        }
-        
-    }
-    public void compareCards(int card, int pid){
-        if(bestCard < 13){
-            if(card < 13 && card > bestCard){
-                bestCard = card;
-                currentWinner = pid;
-            }
-        }
-        else if(bestCard < 26){
-            if(card < 26 && card > bestCard){
-                bestCard = card;
-                currentWinner = pid;
-            }
-        }
-        else if(bestCard < 39){
-            if(card < 39 && card > bestCard){
-                bestCard = card;
-                currentWinner = pid;
-            }
-        }
-        else{
-            if(card > bestCard){
-                bestCard = card;
-                currentWinner = pid;
+            for(int i = 0; i < 4; i++){
+                player4Cards.add(cards.get(i));
             }
         }
     }
+    public String calculateScore(){
+        String score = "";
+        score += calScore(player1Cards, 0);
+        score += calScore(player2Cards, 1);
+        score += calScore(player3Cards, 2);
+        score += calScore(player4Cards, 3);
+        return score;
+    }
+    public String calScore(ArrayList <Integer> cards, int p){
+        int scr = 0;
+        boolean multiply = false;
+        int pig = -100;
+        int goat = 100;
+        int aceH = -50;
+        if(exposedCards.contains(10)){
+            pig = -200;
+        }
+        if(exposedCards.contains(35)){
+            goat = 200;
+        }
+        if(exposedCards.contains(51)){
+            aceH = -100;
+        }
+        for(int i = 0; i < cards.size(); i++){
+            switch (cards.get(i)){
+                    case 10: scr += pig;
+                    case 21: multiply = true;
+                    case 35: scr += goat;
+                    case 42: scr += -10;
+                    case 43: scr += -10;
+                    case 44: scr += -10;
+                    case 45: scr += -10;
+                    case 46: scr += -10;
+                    case 47: scr += -10;
+                    case 48: scr += -20;
+                    case 49: scr += -30;
+                    case 50: scr += -40;
+                    case 51: scr += aceH;
+            }
+        }
+        if(multiply == true){
+            scr = scr * 2;
+            if(exposedCards.contains(21)){
+                scr = scr * 2;
+            }
+        }
+        playerScores[p] += scr;
+        String score = "Player " + p + ": " + playerScores[p] + "   ";
+        return score;
+    }
+    
+    
+    }
+   
 
-}
