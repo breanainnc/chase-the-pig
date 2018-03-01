2faaf86312fd0c3b320546b5b3b8dc8c1948f626
breanainnc <breanainnc@live.ie>
2018-03-01 11:40:54 +0000

    Create Dealer.java

---
+++ GuiClient/Logic/Dealer.java
@@ -0,0 +1,30 @@
+package Logic;
+import GUI.HandPane;
+/**
+ *
+ * @author brean
+ */
+public class Dealer {
+    private HandPane PlayerHand;
+    
+    public Dealer(HandPane PlayerHand){
+        this.PlayerHand = PlayerHand;
+    }
+    public void sendHand(String hand){
+        System.out.println(hand);
+        int start = 0;
+        int end = 2;
+        for(int i = 0; i < 5; i++ ){
+            int card = Integer.parseInt(hand.substring(start, end));
+
+            PlayerHand.addCard(card);
+            start = start + 2;
+            end = end + 2;
+        }
+    }
+
+    public void playerNumber(char number) {
+        String playerNumber = "Player: " + number;
+        PlayerHand.setPlayerNumber(playerNumber);
+    }
+}
