d9eda0e298f09fb45ad1b034fc143f197f584e26
breanainnc <breanainnc@live.ie>
2018-03-08 20:06:17 +0000

    Create CTPServer.java

---
+++ CTPServer/CTPServer.java
@@ -0,0 +1,16 @@
+package ctpserver;
+
+/**
+ *
+ * @author brean
+ */
+public class CTPserver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new SocketServer().runServer();
+    }
+    
+}
