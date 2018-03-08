bbc2244ff2b8ef5e9e89d09d2d1a323d36bcf086
breanainnc <breanainnc@live.ie>
2018-03-08 19:59:41 +0000

    Update Dealer.java

--- GuiClient/Logic/Dealer.java
+++ GuiClient/Logic/Dealer.java
@@ -6,15 +6,19 @@ import GUI.HandPane;
  */
 public class Dealer {
     private HandPane PlayerHand;
+    private playerManager pm;
     
-    public Dealer(HandPane PlayerHand){
+    public Dealer(HandPane PlayerHand,playerManager pm){
         this.PlayerHand = PlayerHand;
+        this.pm = pm;
     }
-    public void sendHand(String hand){
-        System.out.println(hand);
+    
+    //SEND HANDS TO HANDPANE
+    public void sendHand(String hand) throws InterruptedException{
+
         int start = 0;
         int end = 2;
-        for(int i = 0; i < 5; i++ ){
+        for(int i = 0; i < 13; i++ ){
             int card = Integer.parseInt(hand.substring(start, end));
 
             PlayerHand.addCard(card);
@@ -23,8 +27,35 @@ public class Dealer {
         }
     }
 
+    // TELL PLAYER NUMBER 
     public void playerNumber(char number) {
         String playerNumber = "Player: " + number;
         PlayerHand.setPlayerNumber(playerNumber);
+        int n = (int)(number- '0');
+        System.out.println(n);
+        pm.setPlayers(n);
+        
+    }
+    
+    public void cardExposure(String cardsAndId) throws InterruptedException{
+         int start = 0;
+         int end = 3;
+            for(int i = 0; i < 4; i++){
+                int cardPlusId = Integer.parseInt(cardsAndId.substring(start, end));
+                
+                if(cardPlusId != 0){
+                    int Id = cardPlusId % 10;
+                    int card = cardPlusId / 10;
+                    //WORKS
+                    pm.addExCard(Id,card);
+                }
+                start += 3;
+                end += 3;
+            }
+        }
+        
+    
+    public void setPlayerPositions(int pid){
+        
     }
 }
