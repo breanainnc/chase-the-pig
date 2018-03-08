2e959f99d2eb172275987cd0a2058367685a511d
breanainnc <breanainnc@live.ie>
2018-03-08 20:05:02 +0000

    Update MessageSender.java

--- GuiClient/ctpClient/MessageSender.java
+++ GuiClient/ctpClient/MessageSender.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ctpclient;
+
+import java.io.PrintWriter;
 
 /**
  *
  * @author brean
  */
 public class MessageSender {
- 
+    PrintWriter out;
+    
+    public MessageSender(PrintWriter out){
+        this.out = out;
+    }
+    
+    public void expose(int card){
+        if(card == 0){
+            out.println("NOREVEAL");
+        }
+        else{
+            out.println("EXPOSE" + card);
+        }
+    }
 }
