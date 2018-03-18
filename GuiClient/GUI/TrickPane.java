96a48563a56e6941d63b36f534322743d268bd29
breanainnc <breanainnc@live.ie>
2018-03-18 17:11:35 +0000

    Update TrickPane.java

--- GuiClient/GUI/TrickPane.java
+++ GuiClient/GUI/TrickPane.java
@@ -1,11 +1,11 @@
 package GUI;
 
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -17,9 +17,10 @@ import javafx.scene.text.Text;
 public class TrickPane extends Pane{
     final private int cardWidth = 40, cardHeight = 50;
     final private Image cardBack = new Image("Images/blueBackground.png");
-    private ImageView yourCard,leftPlayerCard,rightPlayerCard,frontPlayerCard;
+    private ImageView yourCard,leftPlayerCard,rightPlayerCard,topPlayerCard;
     private exposePrompt exposeprompt;
     private boolean exposeSection = true;
+    private Text playPrompt;
     
     public TrickPane(){
         Rectangle r = new Rectangle(250,250);
@@ -30,7 +31,7 @@ public class TrickPane extends Pane{
         yourCard = new ImageView(cardBack);
         leftPlayerCard = new ImageView(cardBack);
         rightPlayerCard = new ImageView(cardBack);
-        frontPlayerCard = new ImageView(cardBack);
+        topPlayerCard = new ImageView(cardBack);
         
         //SETS FIT SIZE OFF CARDBACK IMAGES
         yourCard.setFitHeight(cardHeight);
@@ -39,8 +40,8 @@ public class TrickPane extends Pane{
         leftPlayerCard.setFitWidth(cardWidth);
         rightPlayerCard.setFitHeight(cardHeight);
         rightPlayerCard.setFitWidth(cardWidth);
-        frontPlayerCard.setFitHeight(cardHeight);
-        frontPlayerCard.setFitWidth(cardWidth);
+        topPlayerCard.setFitHeight(cardHeight);
+        topPlayerCard.setFitWidth(cardWidth);
         
         //SETS THE POSITIONS OF EACH CARD
         yourCard.setX(105);
@@ -49,10 +50,18 @@ public class TrickPane extends Pane{
         leftPlayerCard.setY(100);
         rightPlayerCard.setX(180);
         rightPlayerCard.setY(100);
-        frontPlayerCard.setX(105);
-        frontPlayerCard.setY(25);
+        topPlayerCard.setX(105);
+        topPlayerCard.setY(25);
+  
+        playPrompt = new Text("Not Your Turn");
+        playPrompt.setFill(Color.GOLDENROD);
+        gameplayPrompt  gpp = new gameplayPrompt(playPrompt);
+        gpp.setTranslateY(230);
+
+
         
-        getChildren().addAll(yourCard,leftPlayerCard,rightPlayerCard,frontPlayerCard);
+        
+        getChildren().addAll(yourCard,leftPlayerCard,rightPlayerCard,topPlayerCard,gpp);
         
         exposeprompt = new exposePrompt();
         
@@ -60,11 +69,43 @@ public class TrickPane extends Pane{
         
         
     }
-    public void cardPlayed(int i){
+    public void leftcardPlayed(int i){
         String card = "Images/" + i + ".png";
-        yourCard.setImage(new Image(card));
+        leftPlayerCard.setImage(new Image(card));
         
     }
+     public void rightcardPlayed(int i){
+        String card = "Images/" + i + ".png";
+        rightPlayerCard.setImage(new Image(card));
+        
+    }
+    public void topcardPlayed(int i){
+        String card = "Images/" + i + ".png";
+        topPlayerCard.setImage(new Image(card));
+        
+    }
+    public void yourcardPlayed(int i){
+        String card = "Images/" + i + ".png";
+        yourCard.setImage(new Image(card));
+    }
+    public void setDefaultCardBack() throws InterruptedException {
+        Thread.sleep(2000);
+         //ONLY CALLED BY TASK
+        //SO MUST HAPPEN ON APPLICATION THREAD
+        Platform.runLater(new Runnable() {
+            @Override
+                public void run() {
+                topPlayerCard.setImage(cardBack);
+                yourCard.setImage(cardBack);
+                leftPlayerCard.setImage(cardBack);
+                rightPlayerCard.setImage(cardBack);
+                }});
+    }
+    
+    public void updatePlayPrompt(String message){
+        playPrompt.setText(message);
+    }
+    
     public void exposePrompt(){
         
         if (!getChildren().contains(exposeprompt)){
@@ -77,6 +118,33 @@ public class TrickPane extends Pane{
 }
     public boolean getExposeSection(){
         return exposeSection;
+    }
+    public void setExposeSection(){
+        if(exposeSection == true){
+            exposeSection = false;
+        }
+        else if(exposeSection == false){
+            exposeSection = true;
+        }
+    }
+
+
+    
+    
+    private static class gameplayPrompt extends StackPane {
+        private Text prompt;
+        public gameplayPrompt(Text prompt){
+            this.prompt = prompt;
+            Rectangle base = new Rectangle(250, 20);
+            base.setArcHeight(25);
+            base.setArcWidth(25);
+            
+            
+            getChildren().addAll(base,prompt);
+        }
+        public void setText(String prompt){
+            this.prompt.setText(prompt);
+        }
     }
 
         //STATIC CLASS FOR BACKGROUND DESIGN FOR MAIN MENU
@@ -103,4 +171,5 @@ public class TrickPane extends Pane{
             getChildren().addAll(exposePrompt,line1,pig,goat,aceH,tenC);
     }
  }
+
 }
